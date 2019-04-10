08a67c9356fddbb4d8fcefaf8b6005181ed419b1
Ashish Garg <Ashish.Garg@Honeywell.com>
2019-04-10 14:38:59 +0530

    Improved Factory Pattern class

--- MyDesignPattern/MyDesignPattern/Program.cs
+++ MyDesignPattern/MyDesignPattern/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MyDesignPattern.Creational.FactoryMethod;
+using MyDesignPattern.Creational.FactoryMethod.Enums;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +10,44 @@ namespace MyDesignPattern
 {
     class Program
     {
+        private static TestDifferentDesignPatterns objTestDesignPatterns; 
         static void Main(string[] args)
         {
+            objTestDesignPatterns = new TestDifferentDesignPatterns();
+
+            // Test Factory method pattern
+            objTestDesignPatterns.FactoryMethodClient();
         }
     }
+
+    public class TestDifferentDesignPatterns
+    {
+        public void FactoryMethodClient()
+        {
+            //Console.WriteLine("Below are the multiple factories available who creates Dining Halls:");
+            //Console.WriteLine("S.No.    Factory Name    Factory Code");
+            //Console.WriteLine("1        ABC             0");
+            //Console.WriteLine("1        XYZ             1");
+            //Console.WriteLine("Which Interior Factory you want to create Dinning Hall for you? -- Type the code here");
+
+            //Console.WriteLine(Console.Read());
+
+            //int factType = InteriorFactoryType.ABC.GetHashCode();
+            //Console.WriteLine(factType.ToString());
+
+            // Use ABCInteriorFactory
+            InteriorCreator objInteriorCreator = new ABCInteriorCreator();
+            IInteriorProduct interiorObj = objInteriorCreator.CreateInterior();
+            Console.WriteLine(interiorObj.MakeDiningHall());
+            Console.ReadKey();
+
+            // Use XYZInteriorFactory
+            objInteriorCreator = new XYZInteriorCreator();
+            interiorObj = objInteriorCreator.CreateInterior();
+            Console.WriteLine(interiorObj.MakeDiningHall());
+            Console.ReadKey();
+        }
+    }
+        
+
 }
